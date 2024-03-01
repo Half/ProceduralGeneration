10b4888f13dce317c695e13335086eec347b49b9
Half <jeremy@bulwarkstudios.com>
2024-03-01 12:01:38 +0100

    Terrain scriptable

--- Assets/Exo4_TerrainGeneratorScriptableObject/Scripts/TerrainBiome.cs
+++ Assets/Exo4_TerrainGeneratorScriptableObject/Scripts/TerrainBiome.cs
@@ -3,9 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
-using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
+[CreateAssetMenu(menuName = "Terrain Biome", fileName = "Terrain Biome")]
 public class TerrainBiome : ScriptableObject {
 
     
@@ -18,33 +18,53 @@ public class TerrainBiome : ScriptableObject {
     [SerializeField, Range(0f, 1f)]
     private float validIfGreaterThan;
 
-    public float2 GetNoiseOffset() {
-        
+    public void Generate(ref TerrainBiome[,] terrain) {
 
+        // Setup temp noise array
+        float[,] noiseValues = new float[terrain.GetLength(0), terrain.GetLength(1)];
+        for (int x = 0; x < noiseValues.GetLength(0); x++) {
+            for (int y = 0; y < noiseValues.GetLength(1); y++) {
+                noiseValues[x, y] = 1f;
+            }
+        }
 
-    }
-
-    public bool IsValid(int x, int y) {
-
-        float noiseValue = 1f;
-
+        // Get all noise values
         foreach (TerrainBiomeNoise noise in noises) {
 
             float2 noiseOffset = new float2(Random.Range(noise.offsetMinMax.x, noise.offsetMinMax.y), Random.Range(noise.offsetMinMax.x, noise.offsetMinMax.y));
             float noiseScale = Random.Range(noise.scaleMinMax.x, noise.scaleMinMax.y);
 
-            float2 coordsOffseted = new float2(x + noiseOffset.x, y + noiseOffset.y);
-            float2 coords = new float2(coordsOffseted.x / noiseScale, coordsOffseted.y / noiseScale);
+            for (int x = 0; x < noiseValues.GetLength(0); x++) {
+                for (int y = 0; y < noiseValues.GetLength(1); y++) {
 
-            noiseValue *= noise.GetNoiseValue(coords);
+                    float2 coordsOffseted = new float2(x + noiseOffset.x, y + noiseOffset.y);
+                    float2 coords = new float2(coordsOffseted.x / noiseScale, coordsOffseted.y / noiseScale);
+
+                    noiseValues[x, y] *= noise.GetNoiseValue(coords);
+
+                }
+            }
 
         }
 
-        if (noiseValue >= validIfGreaterThan) {
-            return true;
+        // Apply noise on terrain
+        for (int x = 0; x < terrain.GetLength(0); x++) {
+            for (int y = 0; y < terrain.GetLength(1); y++) {
+
+                if (noiseValues[x, y] >= validIfGreaterThan) {
+
+                    terrain[x, y] = this;
+
+                }
+
+            }
         }
 
-        return false;
+    }
+
+    public GameObject GetPrefabToSpawn() {
+
+        return spawnablePrefabs[Random.Range(0, spawnablePrefabs.Count)];
 
     }
 
--- Assets/Exo4_TerrainGeneratorScriptableObject/Scripts/TerrainGeneratorWithScriptableObject.cs
+++ Assets/Exo4_TerrainGeneratorScriptableObject/Scripts/TerrainGeneratorWithScriptableObject.cs
@@ -45,48 +45,13 @@ public class TerrainGeneratorWithScriptableObject : MonoBehaviour {
         // Init seed
         Random.InitState(seed);
 
+        // Generate terrain
         foreach (TerrainBiome biome in biomes) {
-            
-
-
+            biome.Generate(ref terrain);
         }
 
-        // Mountain
-        Generate(TILE_TYPE.MOUNTAIN, new float2(20f, 40f), NOISE_TYPE.PERLIN, 0.8f);
-
-        // Forest
-        Generate(TILE_TYPE.FOREST, new float2(20f, 40f), NOISE_TYPE.SIMPLEX, 0.5f);
-
-        // Water
-        Generate(TILE_TYPE.WATER, new float2(50f, 70f), NOISE_TYPE.CELLULAR, 0.8f);
-
+        // Spawn
         Spawn();
-
-    }
-
-    private void Generate(TILE_TYPE type, float2 scaleMinMax, NOISE_TYPE noiseType, float greaterThanValue) {
-
-        // Forest
-        float2 noiseOffset = new float2(Random.Range(0f, 100000f), Random.Range(0f, 100000f));
-        float noiseScale = Random.Range(scaleMinMax.x, scaleMinMax.y);
-
-        for (int x = 0; x < terrainSizeX; x++)
-        {
-            for (int y = 0; y < terrainSizeZ; y++)
-            {
-
-                float2 coordsOffseted = new float2(x + noiseOffset.x, y + noiseOffset.y);
-                float2 coords = new float2(coordsOffseted.x / noiseScale, coordsOffseted.y / noiseScale);
-
-                float noiseValue = GetNoiseValue(noiseType, coords);
-
-                if (noiseValue > greaterThanValue)
-                {
-                    terrain[x, y] = type;
-                }
-
-            }
-        }
 
     }
 
@@ -96,60 +61,15 @@ public class TerrainGeneratorWithScriptableObject : MonoBehaviour {
             Destroy(spawnedObject);
         }
 
-        for (int x = 0; x < terrainSizeX; x++)
-        {
+        for (int x = 0; x < terrainSizeX; x++) {
             for (int y = 0; y < terrainSizeZ; y++) {
 
-                GameObject prefab = GetPrefab(terrain[x, y]);
+                GameObject prefab = terrain[x, y].GetPrefabToSpawn();
                 GameObject go = Instantiate(prefab, new Vector3(x, 0f, y), Quaternion.identity);
                 spawnedObjects.Add(go);
 
             }
         }
-
-    }
-
-    private GameObject GetPrefab(TILE_TYPE type) {
-
-        switch (type) {
-            case TILE_TYPE.GRASS:
-                return prGrass;
-            case TILE_TYPE.MOUNTAIN:
-                return prMountain;
-            case TILE_TYPE.FOREST:
-                return prForest;
-            case TILE_TYPE.WATER:
-                return prWater;
-        }
-
-        return null;
-
-    }
-
-    private float GetNoiseValue(NOISE_TYPE noiseType, float2 coords)
-    {
-
-        float value = 0.5f;
-
-        switch (noiseType)
-        {
-            case NOISE_TYPE.CELLULAR:
-                float2 res = noise.cellular(coords);
-                value = res.x;
-                break;
-
-            case NOISE_TYPE.PERLIN:
-                value = noise.cnoise(coords);
-                break;
-
-            case NOISE_TYPE.SIMPLEX:
-                value = noise.snoise(coords);
-                break;
-        }
-
-        value = math.remap(-1f, 1f, 0f, 1f, value);
-
-        return value;
 
     }
 
