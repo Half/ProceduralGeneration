1d87a6e16588bd70ad7c9722eec65419d98d1561
Half <jeremy.guery@gmail.com>
2024-03-07 15:00:02 +0100

    Polish biome

--- Assets/Exo5_6_TerrainGeneratorScriptableObject/Scripts/TerrainBiomeNoise.cs
+++ Assets/Exo5_6_TerrainGeneratorScriptableObject/Scripts/TerrainBiomeNoise.cs
@@ -17,9 +17,11 @@ public struct TerrainBiomeNoise {
 
     [SerializeField] public float2 offsetMinMax;
 
+    [SerializeField] public bool invert;
+
     public float GetNoiseValue(float2 coords) {
 
-        float value = 0.5f;
+        float value = 0f;
 
         switch (noiseType) {
             case TYPE.CELLULAR:
@@ -37,6 +39,10 @@ public struct TerrainBiomeNoise {
         }
 
         value = math.remap(-1f, 1f, 0f, 1f, value);
+
+        if (invert) {
+            value = 1f - value;
+        }
 
         return value;
 
